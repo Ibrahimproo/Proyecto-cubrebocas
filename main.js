2aeb05e65c298b75a91004b3b118cd5466358327
Ibrahimproo <122324317+Ibrahimproo@users.noreply.github.com>
2023-11-01 17:56:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+prediction_1 = ""
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/GPH28cBjR/model.json", modelLoaded);
+
+
+
+
+function modelLoaded() {
+    console.log('¡Modelo cargado!');
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("status").innerHTML=results[0].label;
+    }
+
+}
